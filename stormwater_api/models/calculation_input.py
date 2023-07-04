bcea4c6b67529d82c9cd64c57bd4a8e8de3c609a
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-07-04 07:57:09 +0200

    Add validation enums

--- stormwater_api/models/calculation_input.py
+++ stormwater_api/models/calculation_input.py
@@ -1,9 +1,10 @@
 import hashlib
 import json
+from enum import auto
 
-from pydantic import Field
+from pydantic import Field, validator
 
-from stormwater_api.models.base import BaseModelStrict
+from stormwater_api.models.base import BaseModelStrict, StrEnum
 
 
 def hash_dict(dict_) -> str:
@@ -11,15 +12,30 @@ def hash_dict(dict_) -> str:
     return hashlib.md5(dict_str.encode()).hexdigest()
 
 
+class FlowPath(StrEnum):
+    blockToStreet = auto()
+    blockToPark = auto()
+
+
+class Roofs(StrEnum):
+    extensive = auto()
+    intensive = auto()
+
+
 class Scenario(BaseModelStrict):
     return_period: int = Field(..., alias="returnPeriod")
-    flow_path: str = Field(..., alias="flowPath")
-    roofs: str
+    flow_path: FlowPath = Field(..., alias="flowPath")
+    roofs: Roofs
     model_updates: list[dict] | None = Field(None, alias="modelUpdates")
 
     @property
     def input_filename(self) -> str:
         return f"{self.flow_path}_{self.roofs}_{self.return_period}.inp"
+
+    @validator("return_period")
+    def validate_return_period(cls, v) -> int:
+        assert v in [2, 10, 100], "Return period must be on of [2, 10, 100]"
+        return v
 
 
 class CalculationInput(Scenario):
