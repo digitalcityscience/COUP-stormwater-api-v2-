661baf72f83b058c813b73a359ea6b154065b92d
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-07-11 07:02:17 +0200

    Implement fetch task endpoint

--- stormwater_api/api/main.py
+++ stormwater_api/api/main.py
@@ -2,12 +2,14 @@ import io
 from typing import Union
 
 import uvicorn
+from celery.result import AsyncResult
 from fastapi import Depends, FastAPI, HTTPException, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
 
-from stormwater_api.celery import compute_task
+import stormwater_api.celery as tasks
+from stormwater_api.celery import celery_app
 from stormwater_api.dependencies import city_pyo_client
 from stormwater_api.models.calculation_input import (
     CalculationInput,
@@ -32,8 +34,24 @@ async def process_swimdocktask(calculation_input: CalculationInput):
         scenario=Scenario(**calculation_input.dict(by_alias=True)),
         subcatchments=user_subcatchments,
     )
-    compute_task(processed_input)
-    return processed_input
+    result = tasks.compute_task.delay(jsonable_encoder(processed_input))
+    return {"taskId": result.id}
+
+
+@app.get("/tasks/{task_id}")
+async def get_task(task_id: str):
+    async_result = AsyncResult(task_id, app=celery_app)
+
+    response = {
+        "taskId": async_result.id,
+        "taskState": async_result.state,
+        "taskSucceeded": async_result.successful(),
+        "resultReady": async_result.ready(),
+    }
+    if async_result.ready():
+        response["result"] = async_result.get()
+
+    return response
 
 
 if __name__ == "__main__":
