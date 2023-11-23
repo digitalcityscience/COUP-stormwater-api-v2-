f4375fd98a75dce00250013d1be93c8913a3900f
luis.flcnr <31102486+luisfalconeri@users.noreply.github.com>
2023-11-23 18:34:15 +0100

    Rename endpoints

--- stormwater_api/api/endpoints.py
+++ stormwater_api/api/endpoints.py
@@ -10,34 +10,34 @@ from stormwater_api.models.calculation_input import StormwaterCalculationInput
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter(tags=["tasks"])
+router = APIRouter(tags=["jobs"])
 
 
-@router.post("/tasks")
-async def process_swimdocktask(
+@router.post("/execution")
+async def process_job(
     calculation_input: StormwaterCalculationInput,
 ):
     if result := cache.get(key=calculation_input.celery_key):
         logger.info(
             f"Result fetched from cache with key: {calculation_input.celery_key}"
         )
-        return result
+        return {"result": result}
 
     logger.info(
         f"Result with key: {calculation_input.celery_key} not found in cache. Starting calculation ..."
     )
     result = tasks.compute_task.delay(jsonable_encoder(calculation_input))
-    return {"task_id": result.id}
+    return {"job_id": result.id}
 
 
-@router.get("/tasks/{task_id}")
-async def get_task(task_id: str):
-    async_result = AsyncResult(task_id, app=celery_app)
+@router.get("/jobs/{job_id}/results")
+async def get_job_results(job_id: str):
+    async_result = AsyncResult(job_id, app=celery_app)
 
     response = {
-        "task_id": async_result.id,
-        "task_state": async_result.state,
-        "task_succeeded": async_result.successful(),
+        "job_id": async_result.id,
+        "job_state": async_result.state,
+        "job_succeeded": async_result.successful(),
         "result_ready": async_result.ready(),
     }
 
@@ -47,9 +47,9 @@ async def get_task(task_id: str):
     return response
 
 
-@router.get("/tasks/{task_id}/status")
-async def get_task_status(task_id: str):
-    async_result = AsyncResult(task_id, app=celery_app)
+@router.get("/jobs/{job_id}/status")
+async def get_job_status(job_id: str):
+    async_result = AsyncResult(job_id, app=celery_app)
     state = async_result.state
     if state == "FAILURE":
         state = f"FAILURE : {str(async_result.get())}"
